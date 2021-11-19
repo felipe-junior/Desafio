a60d4d93c925979ede14bbdd39150b936e04d743
felipe-junior <felipesantos.tech@gmail.com>
2021-11-19 12:12:18 -0300

    Alguns testes feitos

--- frontend/src/pages/Animals/animals.test.js
+++ frontend/src/pages/Animals/animals.test.js
@@ -1,21 +1,120 @@
 import '@testing-library/jest-dom'
 import {configureStore} from '@reduxjs/toolkit'
-import {getAnimals} from  '../../redux/slice/animal.slice'
+import {getAnimals, postAnimal, statusConsts, updateAnimal} from  '../../redux/slice/animal.slice'
 import animalsReducer from '../../redux/slice/animal.slice'
 import { render, screen } from '@testing-library/react'
+import {httpGetAnimals, httpPostAnimal, httpUpdateAnimal, httpDeleteAnimal} from '../../utils/requests'
+import Animals from './animals'
+import { Provider } from 'react-redux'
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
+
+jest.mock('../../utils/requests', ()=>({
+    httpGetAnimals: jest.fn(),
+    httpPostAnimal: jest.fn(),
+    httpUpdateAnimal: jest.fn(),
+    httpDeleteAnimal: jest.fn()
+}))
 
 
 
-let store = configureStore({
-    reducer:{
-        animals: animalsReducer
-    }
-})
-describe('Teste no componente animais',  ()=>{
+let store;
+describe('Animals slice',  ()=>{
 
-    test('Resposta da requisição get animals', async ()=>{
+    beforeEach(()=>{
+        store = configureStore({
+            reducer:{
+                animals: animalsReducer
+            }
+        })
+    })
+    afterEach(()=>{
+        httpGetAnimals.mockClear()
+    })
+
+    test('if dispatch getAnimals is fullfiled', async ()=>{
+        httpGetAnimals.mockImplementation(()=>Promise.resolve([
+            {
+            id: 1,
+            nome: "totó",
+            tipo: "cachorro",
+            dataNascimento: Date.now(),
+            peso: 10.0
+            }
+        ]))
         
+         await store.dispatch(getAnimals())
+         expect(store.getState().animals.status).toBe(statusConsts.SUCCESS)
+            
+    })
+
+    test('if dispatch getAnimals is pending', async ()=>{
+        const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+        httpGetAnimals.mockImplementation(()=>{
+            return wait(2000)
+        })
         
-        await store.dispatch(getAnimals());
+         store.dispatch(getAnimals())
+         expect(store.getState().animals.status).toBe(statusConsts.LOADING)
+            
+    })
+    test('if dispatch getAnimals was rejected', async ()=>{
+        httpGetAnimals.mockImplementation(()=>Promise.reject({}))
+        await store.dispatch(getAnimals())
+        expect(store.getState().animals.status).toBe(statusConsts.ERROR)     
+    })
+
+    
+    test('if dispatch postAnimal is fullfiled', async ()=>{
+        const animal = {
+            nome: "totó",
+            tipo: "cachorro",
+            dataNascimento: Date.now(),
+            peso: 10.0
+        }
+        httpPostAnimal.mockImplementation(()=>Promise.resolve({...animal, id:1}))
+        await store.dispatch(postAnimal(animal))
+        expect(store.getState().animals.status).toEqual(statusConsts.SUCCESS)
+        expect(store.getState().animals.entities["1"]).toEqual({...animal, id: 1})
+    })
+    test('if dispatch postAnimal was rejected', async ()=>{
+        const animal = {
+            nome: "totó",
+            tipo: "cachorro",
+            dataNascimento: Date.now(),
+            peso: 10.0
+        }
+        httpPostAnimal.mockImplementation(()=>{
+            return Promise.reject({})
+        })
+        await store.dispatch(postAnimal(animal))
+        expect(store.getState().animals.status).toEqual(statusConsts.ERROR)
+    })
+    test('if dispatch postAnimal was rejected and ErrorMsg is visible', async ()=>{
+        const animal = {
+            nome: "totó",
+            tipo: "cachorro",
+            dataNascimento: Date.now(),
+            peso: 10.0
+        }
+        httpPostAnimal.mockImplementation(()=>{
+            return Promise.reject({})
+        })
+        await store.dispatch(postAnimal(animal))
+        
+       //TODO
+    })
+
+    test('if dispatch update is fullfiled', async ()=>{
+        const updatedAnimal = {
+            id: 1,
+            nome: "totó",
+            tipo: "",
+            dataNascimento: Date.now(),
+            peso: 10.0
+        }
+        httpUpdateAnimal.mockImplementation(()=>Promise.resolve(updatedAnimal))
+        await store.dispatch(updateAnimal(1))
+        expect(store.getState().animals.status).toEqual(statusConsts.SUCCESS)
+        expect(store.getState().animals.entities["1"]).toEqual({...updatedAnimal, id: 1})
     })
 })
